19b8a6be2d42e373f74b9385fcc7fc8e4938af4b
dev-chulbuji <dev.chulbuji@gmail.com>
2021-03-21 18:47:44 +0900

    wip

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
+const health = require('@cloudnative/health-connect');
 const port = 3000
-
 const appVersion = process.env.APP_VERSION
+
+let healthCheck = new health.HealthChecker();
+
+app.get('/ready', health.ReadinessEndpoint(healthCheck))
+app.get('/healthz', health.LivenessEndpoint(healthCheck))
 
 app.get('/', (req, res, next) => {
   const sourceIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   res.json(`source ip :: ${sourceIp}`)
-})
-
-app.get('/healthz', (req, res, next) => {
-  res.json(true)
 })
 
 app.listen(port, () => console.log(`app version is ${appVersion} & server is running on ${port}`))
